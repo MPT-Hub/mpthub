a5717cc67e3aea16bf49beb1c4d6f940af419cda
Leandro Gabriel <lassisg@gmail.com>
2020-07-14 09:41:45 +0100

    Added menu to settings.

--- mpt/settings.py
+++ mpt/settings.py
@@ -37,3 +37,81 @@ class Settings():
         self.DEFAULT_TOTAL_FRAMES = 606
         self.DEFAULT_WIDTH_PX = 512
         self.DEFAULT_WIDTH_SI = 160
+
+        self.MENU = {
+            1: {
+                "menu_name": "&File",
+                "menu_items": {
+                    1: {
+                        "submenu_name": "&Open files",
+                        "submenu_value": "Open ImageJ result file(s)",
+                        "submenu_action": "on_mnuImport",
+                        "submenu_visible": True
+                    },
+                    1: {
+                        "submenu_name": "&Save reports",
+                        "submenu_value": "Save analysis report files",
+                        "submenu_action": "on_mnuExport",
+                        "submenu_visible": False
+                    }
+                }
+            },
+            2: {
+                "menu_name": "&Edit",
+                "menu_items": {
+                    1: {
+                        "submenu_name": "App configuration",
+                        "submenu_value": "General configuration",
+                        "submenu_action": "on_mnuGeneral",
+                        "submenu_visible": True,
+                    },
+                    2: {
+                        "submenu_name": "Diffusivity configuration",
+                        "submenu_value": "Diffusivity ranges configuration",
+                        "submenu_action": "on_mnuDiffusivity",
+                        "submenu_visible": True,
+                    },
+                    3: {
+                        "submenu_name": "",
+                        "submenu_value": "",
+                        "submenu_action": "",
+                        "submenu_visible": True,
+                    },
+                    4: {
+                        "submenu_name": "Start analysis",
+                        "submenu_value": "Starts MPT analysis",
+                        "submenu_action": "on_mnuAnalysis",
+                        "submenu_visible": False,
+                    },
+                    5: {
+                        "submenu_name": "",
+                        "submenu_value": "",
+                        "submenu_action": "",
+                        "submenu_visible": True,
+                    },
+                    6: {
+                        "submenu_name": "Clear summary",
+                        "submenu_value": "Clear current summary list",
+                        "submenu_action": "on_mnuClear",
+                        "submenu_visible": False,
+                    },
+                }
+            },
+            3: {
+                "menu_name": "&Help",
+                "menu_items": {
+                    1: {
+                        "submenu_name": "&Documentation",
+                        "submenu_value": "Application documentation",
+                        "submenu_action": "on_mnuHelp",
+                        "submenu_visible": False
+                    },
+                    2: {
+                        "submenu_name": "&About",
+                        "submenu_value": "About this program",
+                        "submenu_action": "on_mnuAbout",
+                        "submenu_visible": False
+                    },
+                }
+            }
+        }
