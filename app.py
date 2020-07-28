3665bfc4c8f788a0f2c6578b923dab96dc381a0e
Leandro Gabriel <lassisg@gmail.com>
2020-07-28 13:23:52 +0100

    Added Flask to get webGUI.

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
+from flask import Flask, render_template
+from flaskwebgui import FlaskUI  # get the FlaskUI class
+
+
+server = Flask(__name__)
+ui = FlaskUI(server, maximized=True)
+# ui = FlaskUI(server, maximized=True,
+#              browser_path="./chrome/chrome.exe")
 # df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
 
 df = pd.read_csv('C:/Users/lga/OneDrive/Pessoal/Acadêmico/FEUP/2019-2020-PDISS_DISS/_research/Results/20200218/ImageJ/561_H2O_200ul/561_D2_H2O_200ul_Series001_RAW_ch00.csv')
@@ -31,7 +39,8 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    ui.run()
+    app.run_server(debug=False)
 
 # if __name__ == "__main__":
 
