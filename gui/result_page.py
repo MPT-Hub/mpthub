93152936d0a4ea3b7f289c1d21daa359b7ba1fa6
Leandro Gabriel <lassisg@gmail.com>
2020-12-12 17:38:25 +0000

    Updated layout for results page.

--- gui/result_page.py
+++ gui/result_page.py
@@ -1,4 +1,5 @@
 import dash_html_components as html
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 from gui.navbar import Navbar
 from gui.footer import Footer
@@ -9,28 +10,80 @@ sidebar = Sidebar("/result")
 footer = Footer()
 
 body_content = html.Div(children=[
-    dbc.Row(
-        dbc.Button("Export all reports",
-                   id={'page': 'results', 'type': 'all'},
-                   color="primary", className="text-center"),
-        id="all-reports-div",
-        className="mb-3"),
     dbc.Row(children=[
         dbc.Col(
-            dbc.Button("Export Individual analysis report",
-                       id={'page': 'results', 'type': 'individual'},
-                       color="primary", className="text-center"),
-            id="individual-div", className="text-left", width=4),
+            dbc.Button("Export all reports",
+                       id={'page': 'results', 'type': 'all'},
+                       color="primary", className="text-center"))],
+            id="all-reports-div",
+            className="mb-3 text-right"),
+    dbc.Row(children=[
         dbc.Col(
-            dbc.Button("Export Transport Mode report",
-                       id={'page': 'results', 'type': 'transport-mode'},
-                       color="primary", className="text-center"),
-            id="transport-mode-div", className="text-center", width=4),
-        dbc.Col(
-            dbc.Button("Export Einstein-Stokes report",
-                       id={'page': 'results', 'type': 'einstein-stokes'},
-                       color="primary", className="text-center"),
-            id="einstein-stokes-div", className="text-right", width=4)])])
+            dcc.Tabs(children=[
+                dcc.Tab(label='Individual analysis', children=[
+
+                    dbc.Row(dbc.Col(
+                        dcc.Graph(
+                            figure={'data': [
+                                {'x': [1, 2, 3], 'y': [4, 1, 2],
+                                    'type': 'bar', 'name': 'SF'},
+                                {'x': [1, 2, 3], 'y': [2, 4, 5],
+                                    'type': 'bar', 'name': u'Montréal'}]}),
+                    )),
+
+                    dbc.Row(
+                        dbc.Col(
+                            dbc.Button("Export 1",
+                                       id={'page': 'results',
+                                           'type': 'individual'},
+                                       color="primary",
+                                       className="text-center")),
+                        className="text-right",
+                        style={'margin-top': '-115px'})]),
+
+                dcc.Tab(label='Transport Mode', children=[
+
+                    dbc.Row(dbc.Col(
+                        dcc.Graph(
+                            figure={
+                                'data': [
+                                    {'x': [1, 2, 3], 'y': [1, 4, 1],
+                                     'type': 'bar', 'name': 'SF'},
+                                    {'x': [1, 2, 3], 'y': [1, 2, 3],
+                                     'type': 'bar', 'name': u'Montréal'}]}),
+                    )),
+
+                    dbc.Row(
+                        dbc.Col(
+                            dbc.Button("Export 2",
+                                       id={'page': 'results',
+                                           'type': 'transport-mode'},
+                                       color="primary",
+                                       className="text-center")),
+                        className="text-right",
+                        style={'margin-top': '-115px'})]),
+
+                dcc.Tab(label='Einstein-Stokes', children=[
+
+                    dbc.Row(dbc.Col(
+                        dcc.Graph(
+                            figure={
+                                'data': [
+                                    {'x': [1, 2, 3], 'y': [2, 4, 3],
+                                     'type': 'bar', 'name': 'SF'},
+                                    {'x': [1, 2, 3], 'y': [5, 4, 3],
+                                     'type': 'bar', 'name': u'Montréal'}]}),
+                    )),
+
+                    dbc.Row(
+                        dbc.Col(
+                            dbc.Button("Export 3",
+                                       id={'page': 'results',
+                                           'type': 'einstein-stokes'},
+                                       color="primary",
+                                       className="text-center")),
+                        className="text-right",
+                        style={'margin-top': '-115px'})]), ]))])])
 
 body = html.Div(
     dbc.Row(children=[
